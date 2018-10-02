18104c9ce522230de01d1ea4dd7702858e4e4cdb
danielriley06 <driley3@gmail.com>
2018-10-02 11:14:53 -0400

    Creates main entry file for Slack bot, adds descriptions for commands

---
+++ weatherbot/bot.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'slack-ruby-bot'
+
+SlackRubyBot::Client.logger.level = Logger::WARN
+
+module SlackWeatherBot
+  class Bot < SlackRubyBot::Bot
+    help do
+      title 'Weather Bot'
+      desc 'This bot uses the Darksky API to provide weather forecasts.'
+
+      command 'weather now' do
+        desc 'Gives you your current weather information'
+      end
+
+      command 'weather tomorrow' do
+        desc "Gives you tomorrow's forecast"
+      end
+
+      command 'weather in <location>?' do
+        desc "Gives you tomorrow's forecast"
+      end
+    end
+  end
+end
